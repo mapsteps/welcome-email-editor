4342f881e09643126a8d86736e118dc08d9c775f
Bagus <contactjavas@gmail.com>
2021-05-31 22:03:09 +0700

    user welcome email

--- helpers/class-content-helper.php
+++ helpers/class-content-helper.php
@@ -12,4 +12,66 @@ defined( 'ABSPATH' ) || die( "Can't access directly" );
 /**
  * Class to setup screen helper.
  */
-class Content_Helper {}
+class Content_Helper {
+
+	/**
+	 * Get BuddyPress custom profile fields.
+	 *
+	 * @param int $user_id The user ID.
+	 * @return array
+	 */
+	public function get_bp_user_custom_fields( $user_id ) {
+
+		global $wpdb;
+
+		// TODO: Use BuddyPress function if it exists instead of directly touching wpdb.
+		$sql = 'SELECT f.name, d.value
+				FROM
+					' . $wpdb->prefix . 'bp_xprofile_fields f
+					JOIN ' . $wpdb->prefix . 'bp_xprofile_data d ON (d.field_id = f.id)
+				WHERE d.user_id = ' . $user_id;
+
+		$array = $wpdb->get_results( $sql );
+
+		$assoc_array = array();
+
+		foreach ( $array as $key => $value ) {
+			$assoc_array[ $value->name ] = $value->value;
+		}
+
+		$assoc_array = apply_filters( 'weed_bp_custom_fields', $assoc_array );
+
+		return $assoc_array;
+
+	}
+
+	/**
+	 * Get user's custom fields.
+	 *
+	 * @param int $user_id The user ID.
+	 * @return array
+	 */
+	public function get_user_custom_fields( $user_id ) {
+
+		global $wpdb;
+
+		// TODO: Use get_user_meta instead of directly touching wpdb.
+		$sql = 'SELECT meta_key, meta_value
+				FROM ' . $wpdb->usermeta . '
+				WHERE user_ID = ' . $user_id;
+
+		$meta_items = $wpdb->get_results( $sql );
+
+		$custom_fields = array();
+
+		if ( $meta_items ) {
+			foreach ( $meta_items as $meta_item ) {
+				$custom_fields[ $meta_item->meta_key ] = $meta_item->meta_value;
+			}
+		}
+
+		return $custom_fields;
+
+	}
+
+}
--- modules/settings/inc/wp-new-user-notification.php
+++ modules/settings/inc/wp-new-user-notification.php
@@ -6,6 +6,7 @@
  */
 
 use Weed\Vars;
+use Weed\Helpers\Content_Helper;
 use Weed\Settings\Settings_Output;
 
 if ( ! function_exists( 'wp_new_user_notification' ) ) {
@@ -42,36 +43,53 @@ if ( ! function_exists( 'wp_new_user_notification' ) ) {
 
 		$user = get_userdata( $user_id );
 
+		$settings       = Vars::get( 'settings' );
+		$content_helper = new Content_Helper();
+
 		// The blogname option is escaped with esc_html() on the way into the database in sanitize_option().
 		// We want to reverse this for the plain text arena of emails.
 		$blogname = wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES );
 
-		$current_date   = date_i18n( get_option( 'date_format' ) );
-		$current_time   = date_i18n( get_option( 'time_format' ) );
-		$admin_email    = get_option( 'admin_email' );
-		// $reset_pass_url = wp_login_url() . '?action=rp&key=' . $key . '&login=' . rawurlencode( $user->user_login );
-
-		$sql = 'SELECT meta_key, meta_value
-				FROM ' . $wpdb->usermeta . '
-				WHERE user_ID = ' . $user_id;
-
-		$meta_items = $wpdb->get_results( $sql );
-
-		$custom_fields = array();
-
-		if ( $meta_items ) {
-			foreach ( $meta_items as $meta_item ) {
-				$custom_fields[ $meta_item->meta_key ] = $meta_item->meta_value;
-			}
-		}
-
-		$settings      = Vars::get( 'settings' );
-		$module_output = new Settings_Output();
+		$current_date  = date_i18n( get_option( 'date_format' ) );
+		$current_time  = date_i18n( get_option( 'time_format' ) );
+		$admin_email   = get_option( 'admin_email' );
+		$custom_fields = $content_helper->get_user_custom_fields( $user_id );
 
 		if ( 'user' !== $notify ) {
 			$switched_locale = switch_to_locale( get_locale() );
 
-			$find = array(
+			$default_admin_subject = __( '[%s] New User Registration' );
+
+			$admin_subject_placeholders = array(
+				'[blog_name]',
+				'[site_url]',
+				'[first_name]',
+				'[last_name]',
+				'[user_email]',
+				'[user_login]',
+				'[user_id]',
+				'[date]',
+				'[time]',
+			);
+
+			$admin_subject_values = array(
+				$blogname,
+				network_site_url(),
+				$user->first_name,
+				$user->last_name,
+				$user->user_email,
+				$user->user_login,
+				$user->ID,
+				$current_date,
+				$current_time,
+			);
+
+			$admin_subject = $settings['admin_welcome_email_subject'];
+			$admin_subject = $admin_subject ? $admin_subject : $default_admin_subject;
+			$admin_subject = str_ireplace( $admin_subject_placeholders, $admin_subject_values, $admin_subject );
+			$admin_subject = apply_filters( 'weed_user_welcome_email_subject', $admin_subject );
+
+			$admin_body_placeholders = array(
 				'[blog_name]',
 				'[site_url]',
 				'[first_name]',
@@ -87,10 +105,11 @@ if ( ! function_exists( 'wp_new_user_notification' ) ) {
 				'[plaintext_password]',
 				'[user_password]',
 				'[custom_fields]',
+				'[bp_custom_fields]',
 				'[post_data]',
 			);
 
-			$replace = array(
+			$admin_body_values = array(
 				$blogname,
 				network_site_url(),
 				$user->first_name,
@@ -109,30 +128,32 @@ if ( ! function_exists( 'wp_new_user_notification' ) ) {
 				'<pre>' . print_r( $_REQUEST, true ) . '</pre>', // ! Not recommended.
 			);
 
-			$default_subject = __( '[%s] New User Registration' );
-
 			/* translators: %s: Site title. */
-			$default_message = sprintf( __( 'New user registration on your site %s:' ), $blogname ) . "\r\n\r\n";
+			$default_admin_body = sprintf( __( 'New user registration on your site %s:' ), $blogname ) . "\r\n\r\n";
 			/* translators: %s: User login. */
-			$default_message .= sprintf( __( 'Username: %s' ), $user->user_login ) . "\r\n\r\n";
+			$default_admin_body .= sprintf( __( 'Username: %s' ), $user->user_login ) . "\r\n\r\n";
 			/* translators: %s: User email address. */
-			$default_message .= sprintf( __( 'Email: %s' ), $user->user_email ) . "\r\n";
+			$default_admin_body .= sprintf( __( 'Email: %s' ), $user->user_email ) . "\r\n";
 
-			$subject = $settings['user_welcome_email_subject'];
-			$subject = $subject ? $subject : $default_subject;
-			$subject = str_ireplace( $find, $replace, $subject );
-			$subject = apply_filters( 'weed_user_welcome_email_subject', $subject );
+			$admin_body = $settings['admin_welcome_email_body'];
+			$admin_body = $admin_body ? $admin_body : $default_admin_body;
+			$admin_body = str_ireplace( $admin_body_placeholders, $admin_body_values, $admin_body );
 
-			$message = $settings['user_welcome_email_subject'];
-			$message = $message ? $message : $default_message;
-			$message = str_ireplace( $find, $replace, $message );
-			$message = apply_filters( 'weed_user_welcome_email_message', $message );
+			if ( stripos( $admin_body, '[bp_custom_fields]' ) ) {
+				$admin_body = str_replace(
+					'[bp_custom_fields]',
+					'<pre>' . print_r( $content_helper->get_bp_user_custom_fields( $user_id ), true ) . '</pre>',
+					$admin_body
+				);
+			}
+
+			$admin_body = apply_filters( 'weed_user_welcome_email_body', $admin_body );
 
 			$wp_new_user_notification_email_admin = array(
 				'to'      => get_option( 'admin_email' ),
 				/* translators: New user registration notification email subject. %s: Site title. */
-				'subject' => $subject,
-				'message' => $message,
+				'subject' => $admin_subject,
+				'message' => $admin_body,
 				'headers' => '',
 			);
 
@@ -179,18 +200,90 @@ if ( ! function_exists( 'wp_new_user_notification' ) ) {
 
 		$switched_locale = switch_to_locale( get_user_locale( $user ) );
 
-		/* translators: %s: User login. */
-		$message  = sprintf( __( 'Username: %s' ), $user->user_login ) . "\r\n\r\n";
-		$message .= __( 'To set your password, visit the following address:' ) . "\r\n\r\n";
-		$message .= network_site_url( "wp-login.php?action=rp&key=$key&login=" . rawurlencode( $user->user_login ), 'login' ) . "\r\n\r\n";
+		$reset_pass_url = network_site_url( "wp-login.php?action=rp&key=$key&login=" . rawurlencode( $user->user_login ), 'login' );
 
-		$message .= wp_login_url() . "\r\n";
+		$default_user_subject = __( '[%s] Login Details' );
+
+		$user_subject_placeholders = array(
+			'[blog_name]',
+			'[site_url]',
+			'[first_name]',
+			'[last_name]',
+			'[user_email]',
+			'[user_login]',
+			'[user_id]',
+			'[date]',
+			'[time]',
+		);
+
+		$user_subject_values = array(
+			$blogname,
+			network_site_url(),
+			$user->first_name,
+			$user->last_name,
+			$user->user_email,
+			$user->user_login,
+			$user->ID,
+			$current_date,
+			$current_time,
+		);
+
+		$user_subject = $settings['user_welcome_email_subject'];
+		$user_subject = $user_subject ? $user_subject : $default_user_subject;
+		$user_subject = str_ireplace( $user_subject_placeholders, $user_subject_values, $user_subject );
+		$user_subject = apply_filters( 'weed_user_welcome_email_subject', $user_subject );
+
+		/* translators: %s: User login. */
+		$default_user_body  = sprintf( __( 'Username: %s' ), $user->user_login ) . "\r\n\r\n";
+		$default_user_body .= __( 'To set your password, visit the following address:' ) . "\r\n\r\n";
+		$default_user_body .= $reset_pass_url . "\r\n\r\n";
+		$default_user_body .= wp_login_url() . "\r\n";
+
+		$user_body_placeholders = array(
+			'[blog_name]',
+			'[site_url]',
+			'[first_name]',
+			'[last_name]',
+			'[user_email]',
+			'[user_login]',
+			'[user_id]',
+			'[date]',
+			'[time]',
+			'[admin_email]',
+			'[login_url]',
+			'[reset_pass_url]',
+			'[reset_pass_link]',
+			'[plaintext_password]',
+			'[user_password]',
+		);
+
+		$user_body_values = array(
+			$blogname,
+			network_site_url(),
+			$user->first_name,
+			$user->last_name,
+			$user->user_email,
+			$user->user_login,
+			$user->ID,
+			$current_date,
+			$current_time,
+			$admin_email,
+			wp_login_url(),
+			$reset_pass_url,
+			'<a href="' . $reset_pass_url . '" target="_blank">' . __( 'Click to set', 'welome-email-editor' ) . '</a>',
+			'*****',
+			'*****',
+		);
+
+		$user_body = $settings['user_welcome_email_body'];
+		$user_body = $user_body ? $user_body : $default_user_body;
+		$user_body = str_ireplace( $user_body_placeholders, $user_body_values, $user_body );
 
 		$wp_new_user_notification_email = array(
 			'to'      => $user->user_email,
 			/* translators: Login details notification email subject. %s: Site title. */
-			'subject' => __( '[%s] Login Details' ),
-			'message' => $message,
+			'subject' => $user_subject,
+			'message' => $user_body,
 			'headers' => '',
 		);
 
