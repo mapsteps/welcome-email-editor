f234c381b430aeb29ed8d801e577a650bbd83f79
Bagus <contactjavas@gmail.com>
2021-06-01 21:44:40 +0700

    implement additional / custom headers

--- modules/settings/inc/wp-new-user-notification.php
+++ modules/settings/inc/wp-new-user-notification.php
@@ -58,6 +58,39 @@ if ( ! function_exists( 'wp_new_user_notification' ) ) {
 		$admin_email   = get_option( 'admin_email' );
 		$custom_fields = $content_helper->get_user_custom_fields( $user_id );
 
+		$headers  = array();
+		$reply_to = '';
+
+		if ( ! empty( $values['user_welcome_email_reply_to_email'] ) ) {
+			$reply_to = 'Reply-To:';
+
+			// Only check for reply to name, if reply to email exists.
+			if ( ! empty( $values['user_welcome_email_reply_to_name'] ) ) {
+				$reply_to .= ' ' . $settings['user_welcome_email_reply_to_name'];
+			}
+
+			if ( isset( $settings['user_welcome_email_reply_to_email'] ) ) {
+				$reply_to .= ' <' . $settings['user_welcome_email_reply_to_email'] . '>';
+			}
+		}
+
+		if ( ! empty( $reply_to ) ) {
+			array_push( $headers, $reply_to );
+		}
+
+		$custom_headers = $values['user_welcome_email_additional_headers'];
+		$custom_headers = trim( $custom_headers );
+		$custom_headers = str_ireplace( "\r\n", "\n", $custom_headers );
+		$custom_headers = explode( "\n", $custom_headers );
+
+		foreach ( $custom_headers as $custom_header ) {
+			$custom_header = trim( $custom_header );
+
+			if ( ! empty( $custom_header ) ) {
+				array_push( $headers, $custom_header );
+			}
+		}
+
 		if ( 'user' !== $notify ) {
 			$switched_locale = switch_to_locale( get_locale() );
 
@@ -157,7 +190,7 @@ if ( ! function_exists( 'wp_new_user_notification' ) ) {
 				/* translators: New user registration notification email subject. %s: Site title. */
 				'subject' => $admin_subject,
 				'message' => $admin_body,
-				'headers' => '',
+				'headers' => ! empty( $headers ) ? $headers : '',
 			);
 
 			/**
@@ -202,26 +235,6 @@ if ( ! function_exists( 'wp_new_user_notification' ) ) {
 		}
 
 		$switched_locale = switch_to_locale( get_user_locale( $user ) );
-
-		$headers  = array();
-		$reply_to = '';
-
-		if ( ! empty( $values['user_welcome_email_reply_to_email'] ) ) {
-			$reply_to = 'Reply-To:';
-
-			// Only check for reply to name, if reply to email exists.
-			if ( ! empty( $values['user_welcome_email_reply_to_name'] ) ) {
-				$reply_to .= ' ' . $settings['user_welcome_email_reply_to_name'];
-			}
-
-			if ( isset( $settings['user_welcome_email_reply_to_email'] ) ) {
-				$reply_to .= ' <' . $settings['user_welcome_email_reply_to_email'] . '>';
-			}
-		}
-
-		if ( ! empty( $reply_to ) ) {
-			array_push( $headers, $reply_to );
-		}
 
 		$reset_pass_url = network_site_url( "wp-login.php?action=rp&key=$key&login=" . rawurlencode( $user->user_login ), 'login' );
 
