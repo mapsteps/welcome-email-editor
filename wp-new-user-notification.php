908c045e77633af3b67f8e3ec896098f2f088ae8
Bagus <contactjavas@gmail.com>
2021-06-02 23:41:40 +0700

    let's use values instead of settings

--- wp-new-user-notification.php
+++ wp-new-user-notification.php
@@ -43,7 +43,7 @@ if ( ! function_exists( 'wp_new_user_notification' ) ) {
 
 		$user = get_userdata( $user_id );
 
-		$settings = Vars::get( 'settings' );
+		$values = Vars::get( 'values' );
 
 		$content_helper = new Content_Helper();
 		$email_helper   = new Email_Helper();
@@ -92,7 +92,7 @@ if ( ! function_exists( 'wp_new_user_notification' ) ) {
 				$current_time,
 			);
 
-			$admin_subject = $settings['admin_new_user_notif_email_subject'];
+			$admin_subject = $values['admin_new_user_notif_email_subject'];
 			$admin_subject = $admin_subject ? $admin_subject : $default_admin_subject;
 			$admin_subject = str_ireplace( $admin_subject_placeholders, $admin_subject_values, $admin_subject );
 			$admin_subject = apply_filters( 'weed_user_welcome_email_subject', $admin_subject );
@@ -143,7 +143,7 @@ if ( ! function_exists( 'wp_new_user_notification' ) ) {
 			/* translators: %s: User email address. */
 			$default_admin_body .= sprintf( __( 'Email: %s' ), $user->user_email ) . "\r\n";
 
-			$admin_body = $settings['admin_new_user_notif_email_body'];
+			$admin_body = $values['admin_new_user_notif_email_body'];
 			$admin_body = $admin_body ? $admin_body : $default_admin_body;
 			$admin_body = str_ireplace( $admin_body_placeholders, $admin_body_values, $admin_body );
 
@@ -183,7 +183,7 @@ if ( ! function_exists( 'wp_new_user_notification' ) ) {
 
 			$custom_recipient_emails = array();
 
-			$custom_recipients = $settings['admin_new_user_notif_email_custom_recipients'];
+			$custom_recipients = $values['admin_new_user_notif_email_custom_recipients'];
 			$custom_recipients = trim( $custom_recipients );
 			$custom_recipients = rtrim( $custom_recipients, ',' ); // Make sure there's no trailing comma to prevent double commas.
 
@@ -273,7 +273,7 @@ if ( ! function_exists( 'wp_new_user_notification' ) ) {
 			$current_time,
 		);
 
-		$user_subject = $settings['user_welcome_email_subject'];
+		$user_subject = $values['user_welcome_email_subject'];
 		$user_subject = $user_subject ? $user_subject : $default_user_subject;
 		$user_subject = str_ireplace( $user_subject_placeholders, $user_subject_values, $user_subject );
 		$user_subject = apply_filters( 'weed_user_welcome_email_subject', $user_subject );
@@ -320,7 +320,7 @@ if ( ! function_exists( 'wp_new_user_notification' ) ) {
 			'*****',
 		);
 
-		$user_body = $settings['user_welcome_email_body'];
+		$user_body = $values['user_welcome_email_body'];
 		$user_body = $user_body ? $user_body : $default_user_body;
 		$user_body = str_ireplace( $user_body_placeholders, $user_body_values, $user_body );
 
@@ -350,7 +350,7 @@ if ( ! function_exists( 'wp_new_user_notification' ) ) {
 		 */
 		$wp_new_user_notification_email = apply_filters( 'wp_new_user_notification_email', $wp_new_user_notification_email, $user, $blogname );
 
-		$attachment = $settings['user_welcome_email_attachment_url'];
+		$attachment = $values['user_welcome_email_attachment_url'];
 		$attachment = trim( $attachment );
 		$attachment = empty( $attachment ) ? '' : $attachment;
 
