d5cdbe015c76f9be61d68bced543b87e39191db2
Bagus <contactjavas@gmail.com>
2021-06-03 09:58:53 +0700

    style adajustment

--- modules/settings/templates/settings-template.php
+++ modules/settings/templates/settings-template.php
@@ -90,7 +90,7 @@ return function () {
 					<h2><?php _e( 'Template Tags', 'welome-email-editor' ); ?></h2>
 					<div class="heatbox-content">
 						<p><?php _e( 'Use the template tags below in any fields to output certain information.', 'welcome-email-editor' ); ?></p>
-						<p style="margin-bottom: 0;">
+						<p class="tags-wrapper">
 							<code>[site_url]</code>, <code>[login_url]</code>, <code>[reset_pass_url]</code>, <code>[user_email]</code>, <code>[user_login]</code>, <code>[user_id]</code>, <code>[first_name]</code>, <code>[last_name]</code>, <code>[blog_name]</code>, <code>[admin_email]</code>, <code>[custom_fields]</code>, <code>[date]</code>, <code>[time]</code>
 						</p>
 					</div>
@@ -102,7 +102,7 @@ return function () {
 					<div class="heatbox-content">
 						<p><?php _e( 'Use the template tag below in your <strong>Admin Email</strong>.', 'welcome-email-editor' ); ?></p>
 						<p><?php _e( 'This will print BuddyPress custom fields.', 'welcome-email-editor' ); ?></p>
-						<p style="margin-bottom: 0;">
+						<p class="tags-wrapper">
 							<code>[bp_custom_fields]</code>
 						</p>
 					</div>
@@ -114,7 +114,7 @@ return function () {
 					<div class="heatbox-content">
 						<p><?php _e( 'Use the template tag below in your <strong>Admin Email</strong> for debugging.', 'welcome-email-editor' ); ?></p>
 						<p><?php _e( 'This will print $_REQUEST.', 'welcome-email-editor' ); ?></p>
-						<p style="margin-bottom: 0;">
+						<p class="tags-wrapper">
 							<code>[post_data]</code>
 						</p>
 					</div>
