51c5adb1dfa1b715f68214a0aaec1dcfa0025590
MapSteps <connect@mapsteps.com>
2023-12-31 22:00:55 +0100

    added further explanation

--- modules/settings/templates/metaboxes/test-smtp-metabox.php
+++ modules/settings/templates/metaboxes/test-smtp-metabox.php
@@ -17,9 +17,15 @@ $admin_email = get_bloginfo( 'admin_email' );
 
 		<div class="heatbox-content weed-fields">
 			<div class="weed-field">
-				<label class="weed-label" for="weed-test-smtp" style="margin-bottom: 10px;">
+				<p>
 					<?php esc_html_e( 'Please enter an email address to which the test email should be sent.', 'welcome-email-editor' ); ?>
-				</label>
+				</p>
+				<p>
+					<?php esc_html_e( 'Before sending a test email, please save your settings.', 'welcome-email-editor' ); ?>
+				</p>
+				<p>
+					<?php esc_html_e( 'If you haven\'t received the test email, please check your spam folder and ensure the SMTP settings are configured correctly.', 'welcome-email-editor' ); ?>
+				</p>
 				<input type="email" name="weed_test_smtp" id="weed-test-smtp"
 						value="<?php echo esc_attr( $admin_email ); ?>" class="all-options">
 			</div>
