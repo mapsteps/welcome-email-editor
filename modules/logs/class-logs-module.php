d015a924d45bdf56cc3b67ff7c5f19f5b4641b3f
Bagus <25834188+contactjavas@users.noreply.github.com>
2024-08-30 19:32:17 +0700

    formatting

--- modules/logs/class-logs-module.php
+++ modules/logs/class-logs-module.php
@@ -45,9 +45,9 @@ class Logs_Module extends Base_Module {
 
 		parent::__construct();
 
-		$module = new Settings_Module; 
+		$module = new Settings_Module();
 
-		$this->url = WEED_PLUGIN_URL . '/modules/logs';
+		$this->url      = WEED_PLUGIN_URL . '/modules/logs';
 		$this->settings = $module->settings;
 
 	}
@@ -70,7 +70,7 @@ class Logs_Module extends Base_Module {
 	/**
 	 * Set up the module.
 	 */
-	public function setup() {	
+	public function setup() {
 
 		$is_checked = isset( $this->settings['enable_email_logging'] ) ? 1 : 0;
 
@@ -78,21 +78,21 @@ class Logs_Module extends Base_Module {
 			return;
 		}
 
-		add_action( 'init', array( $this, 'register_email_logs_cpt' ), 20 ); 
- 		add_action( 'admin_menu', array( $this, 'email_logs_submenu' ), 20 );		  
+		add_action( 'init', array( $this, 'register_email_logs_cpt' ), 20 );
+		add_action( 'admin_menu', array( $this, 'email_logs_submenu' ), 20 );
 		add_filter( 'wp_mail', array( $this, 'capture_email_details_for_logging' ), 10, 1 );
-		add_action( 'wp_mail_succeeded', array( $this, 'handle_success_email' ) ); 
+		add_action( 'wp_mail_succeeded', array( $this, 'handle_success_email' ) );
 		add_action( 'wp_mail_failed', array( $this, 'handle_failed_email' ) );
 
 		// The module output.
 		require_once __DIR__ . '/class-logs-output.php';
 		Logs_Output::init();
 
-	} 
+	}
 
 	/**
 	 * Register a Custom Post Type for Email Logs
-	 */ 
+	 */
 	public function register_email_logs_cpt() {
 
 		$labels = array(
@@ -108,21 +108,21 @@ class Logs_Module extends Base_Module {
 			'all_items'          => __( 'All Email Logs', 'welcome-email-editor' ),
 			'search_items'       => __( 'Search Email Logs', 'welcome-email-editor' ),
 			'not_found'          => __( 'No email logs found.', 'welcome-email-editor' ),
-			'not_found_in_trash' => __( 'No email logs found in Trash.', 'welcome-email-editor' )
+			'not_found_in_trash' => __( 'No email logs found in Trash.', 'welcome-email-editor' ),
 		);
 
 		$args = array(
-			'labels'             => $labels,
-			'public'             => false,
-			'show_ui'            => true,
-			'show_in_menu'       => false,
-			'query_var'          => true,
-			'rewrite'            => array( 'slug' => 'email_log' ),
-			'capability_type'    => 'post',
-			'has_archive'        => false,
-			'hierarchical'       => false,
-			'menu_position'      => null,
-			'supports'           => array( 'title', 'editor' )
+			'labels'          => $labels,
+			'public'          => false,
+			'show_ui'         => true,
+			'show_in_menu'    => false,
+			'query_var'       => true,
+			'rewrite'         => array( 'slug' => 'email_log' ),
+			'capability_type' => 'post',
+			'has_archive'     => false,
+			'hierarchical'    => false,
+			'menu_position'   => null,
+			'supports'        => array( 'title', 'editor' ),
 		);
 
 		register_post_type( 'email_logs', $args );
@@ -132,7 +132,7 @@ class Logs_Module extends Base_Module {
 	/**
 	 * Add email logs as sub menu of the SMTP settings.
 	 */
-	public function email_logs_submenu() { 
+	public function email_logs_submenu() {
 
 		add_submenu_page(
 			'weed_settings', // parent slug
@@ -141,21 +141,21 @@ class Logs_Module extends Base_Module {
 			'edit_posts', // capability
 			'edit.php?post_type=email_logs' // your menu menu slug
 		);
-	} 
- 
+	}
+
 	/**
 	 * Hook into wp_mail to capture email details before sending
 	 */
 	public function capture_email_details_for_logging( $args ) {
-	 
+
 		$GLOBALS['current_email_log'] = array(
-			'subject'     => $args['subject'], 
+			'subject'     => $args['subject'],
 			'email_body'  => $args['message'],
-			'recipient'   => is_array( $args['to']) ? implode(', ', $args['to']) : $args['to'],
+			'recipient'   => is_array( $args['to'] ) ? implode( ', ', $args['to'] ) : $args['to'],
 			'headers'     => $args['headers'],
 			'attachments' => $args['attachments'],
 		);
-		
+
 		return $args;
 
 	}
@@ -163,61 +163,71 @@ class Logs_Module extends Base_Module {
 	/**
 	 * Action to handle successful emails.
 	 */
-	public function handle_success_email( $mail_data ) { 
-		 if ( isset( $GLOBALS['current_email_log'] ) ) {
+	public function handle_success_email( $mail_data ) {
+
+		if ( isset( $GLOBALS['current_email_log'] ) ) {
 
 			// Defer the logging to after the email has been attempted to send
 			$this->log_email_event( 'Success' );
 			unset( $GLOBALS['current_email_log'] );
 
 		}
+
 	}
 
 	/**
 	 * Hook into the wp_mail_failed action to handle email failures
+	 *
 	 * @param [type] $wp_error
-	 */ 
-	public function handle_failed_email($wp_error) {
-		if (isset($GLOBALS['current_email_log'])) {
+	 */
+	public function handle_failed_email( $wp_error ) {
+
+		if ( isset( $GLOBALS['current_email_log'] ) ) {
 			$error_message = $wp_error->get_error_message();
 
 			// Log the email as failed
-			$this->log_email_event('Failed', $error_message);
-			unset($GLOBALS['current_email_log']);
+			$this->log_email_event( 'Failed', $error_message );
+			unset( $GLOBALS['current_email_log'] );
 		}
+
 	}
 
 	/**
 	 * Helper function to log email events
 	 */
-	public function log_email_event($status, $error_message = '') {
+	public function log_email_event( $status, $error_message = '' ) {
+
 		$email_log = $this->get_current_email_log();
-		
-		if (!$email_log) {
+
+		if ( ! $email_log ) {
 			return; // If no email log data exists, do nothing
 		}
 
-		$email_type = $this->determine_email_type($email_log['subject']);
+		$email_type = $this->determine_email_type( $email_log['subject'] );
 		$sender     = $this->get_sender_email();
-		
+
 		// Insert the email log as a custom post type entry
-		$this->insert_email_log_post($email_log, $email_type, $sender, $status, $error_message);
+		$this->insert_email_log_post( $email_log, $email_type, $sender, $status, $error_message );
 
 		// Clear the global variable after logging
-		unset($GLOBALS['current_email_log']);
+		unset( $GLOBALS['current_email_log'] );
+
 	}
 
 	/**
 	 * Retrieve current email log from the global variable
 	 */
 	protected function get_current_email_log() {
-		return isset($GLOBALS['current_email_log']) ? $GLOBALS['current_email_log'] : false;
+
+		return isset( $GLOBALS['current_email_log'] ) ? $GLOBALS['current_email_log'] : false;
+
 	}
 
 	/**
 	 * Determine the email type based on the subject
 	 */
-	protected function determine_email_type($subject) {
+	protected function determine_email_type( $subject ) {
+
 		$email_type_mappings = array(
 			'Password'      => 'Reset Password',
 			'New User'      => 'New User',
@@ -225,38 +235,42 @@ class Logs_Module extends Base_Module {
 			'Test Email'    => 'Test Email',
 		);
 
-		foreach ($email_type_mappings as $keyword => $type) {
-			if (strpos($subject, $keyword) !== false) {
+		foreach ( $email_type_mappings as $keyword => $type ) {
+			if ( strpos( $subject, $keyword ) !== false ) {
 				return $type;
 			}
 		}
 
 		return 'General';
+
 	}
 
 	/**
 	 * Get the sender email address
 	 */
 	protected function get_sender_email() {
-		$force_from_email = isset($this->settings['force_from_email']) ? 1 : 0;
 
-		if ($force_from_email && isset($this->settings['from_email'])) {
+		$force_from_email = isset( $this->settings['force_from_email'] ) ? 1 : 0;
+
+		if ( $force_from_email && isset( $this->settings['from_email'] ) ) {
 			return $this->settings['from_email'];
 		}
 
-		return get_bloginfo('admin_email');
+		return get_bloginfo( 'admin_email' );
+
 	}
 
 	/**
 	 * Insert the email log as a custom post type entry
 	 */
-	protected function insert_email_log_post($email_log, $email_type, $sender, $status, $error_message) {
+	protected function insert_email_log_post( $email_log, $email_type, $sender, $status, $error_message ) {
+
 		wp_insert_post(array(
 			'post_title'   => $email_log['subject'],
 			'post_type'    => 'email_logs',
 			'post_status'  => 'publish',
 			'post_content' => $email_log['email_body'],
-			'post_date'    => current_time('mysql'),
+			'post_date'    => current_time( 'mysql' ),
 			'meta_input'   => array(
 				'email_type'          => $email_type,
 				'sender'              => $sender,
@@ -265,8 +279,7 @@ class Logs_Module extends Base_Module {
 				'email_error_message' => $error_message,
 			),
 		));
-	}
 
- 
+	}
 
 }
--- modules/logs/class-logs-output.php
+++ modules/logs/class-logs-output.php
@@ -6,8 +6,8 @@
  */
 
 namespace Weed\Logs;
- 
-use Weed\Base\Base_Output; 
+
+use Weed\Base\Base_Output;
 
 defined( 'ABSPATH' ) || die( "Can't access directly" );
 
@@ -68,32 +68,34 @@ class Logs_Output extends Base_Output {
 	 * Setup Logging output.
 	 */
 	public function setup() {
- 
+
 		add_filter( 'manage_email_logs_posts_columns', array( $this, 'set_custom_email_logs_columns' ) );
 		add_action( 'manage_email_logs_posts_custom_column', array( $this, 'custom_email_logs_column' ), 10, 2 );
 		add_action( 'restrict_manage_posts', array( $this, 'filter_email_logs_by_status' ) );
 		add_action( 'pre_get_posts', array( $this, 'filter_email_logs_query_by_status' ) );
-		add_action( 'admin_head', array( $this, 'custom_email_logs_status_styles' ) ); 
+		add_action( 'admin_head', array( $this, 'custom_email_logs_status_styles' ) );
 		add_action( 'current_screen', array( $this, 'restrict_access_to_email_logs' ) );
-		add_action( 'admin_menu', array( $this, 'hide_email_logs_menu' ), 999 ); 
+		add_action( 'admin_menu', array( $this, 'hide_email_logs_menu' ), 999 );
 		add_action( 'add_meta_boxes', array( $this, 'add_email_logs_metabox' ) );
 	}
-  
+
 	/**
 	 * Display Custom Columns in the Email Logs List Table
 	 *
 	 * @param $columns.
 	 */
-	public function set_custom_email_logs_columns($columns) {
-		unset($columns['date']);
+	public function set_custom_email_logs_columns( $columns ) {
 
-		$columns['email_type'] = __('Email Type', 'welcome-email-editor');
-		$columns['sender']     = __('Sender', 'welcome-email-editor');
-		$columns['recipient']  = __('Recipient', 'welcome-email-editor');
-		$columns['status']     = __('Status', 'welcome-email-editor');
-		$columns['date']       = __('Date/Time', 'welcome-email-editor');
+		unset( $columns['date'] );
+
+		$columns['email_type'] = __( 'Email Type', 'welcome-email-editor' );
+		$columns['sender']     = __( 'Sender', 'welcome-email-editor' );
+		$columns['recipient']  = __( 'Recipient', 'welcome-email-editor' );
+		$columns['status']     = __( 'Status', 'welcome-email-editor' );
+		$columns['date']       = __( 'Date/Time', 'welcome-email-editor' );
 
 		return $columns;
+
 	}
 
 	/**
@@ -101,56 +103,60 @@ class Logs_Output extends Base_Output {
 	 *
 	 * @param $column, $post_id.
 	 */
-	public function custom_email_logs_column($column, $post_id) {
-		switch ($column) {
+	public function custom_email_logs_column( $column, $post_id ) {
+
+		switch ( $column ) {
 			case 'email_type':
-				echo esc_html(get_post_meta($post_id, 'email_type', true));
+				echo esc_html( get_post_meta( $post_id, 'email_type', true ) );
 				break;
 			case 'sender':
-				echo esc_html(get_post_meta($post_id, 'sender', true));
-				break; 
+				echo esc_html( get_post_meta( $post_id, 'sender', true ) );
+				break;
 			case 'recipient':
-				echo esc_html(get_post_meta($post_id, 'recipient', true));
+				echo esc_html( get_post_meta( $post_id, 'recipient', true ) );
 				break;
 			case 'status':
-				$status = get_post_meta($post_id, 'status', true);
+				$status = get_post_meta( $post_id, 'status', true );
 				// Conditionally style the status
-				if ($status == 'Success') {
-					echo '<span style="color: green; font-weight: bold;">' . esc_html($status) . '</span>';
-				} elseif ($status == 'Failed') {
-					echo '<span style="color: red; font-weight: bold;">' . esc_html($status) . '</span>';
+				if ( $status == 'Success' ) {
+					echo '<span style="color: green; font-weight: bold;">' . esc_html( $status ) . '</span>';
+				} elseif ( $status == 'Failed' ) {
+					echo '<span style="color: red; font-weight: bold;">' . esc_html( $status ) . '</span>';
 				} else {
-					echo esc_html($status);
+					echo esc_html( $status );
 				}
 				break;
-			case 'date': 
-				echo esc_html(get_the_date('', $post_id));
+			case 'date':
+				echo esc_html( get_the_date( '', $post_id ) );
 				break;
 		}
+
 	}
 
 	/**
 	 * Add inline CSS for better styling.
 	 */
 	public function custom_email_logs_status_styles() {
+
 		echo '
 		<style>
 			.column-email_status { width: 150px; }
 		</style>
 		';
+
 	}
 
 	/**
 	 * Add the filter dropdown to the admin list view
-	 * 
-	 */ 
+	 */
 	public function filter_email_logs_by_status() {
+
 		global $typenow;
 
 		// Only add the filter for the 'email_logs' post type
 		if ( $typenow == 'email_logs' ) {
-			$selected = isset($_GET['email_status']) ? $_GET['email_status'] : '';
-			
+			$selected = isset( $_GET['email_status'] ) ? $_GET['email_status'] : '';
+
 			// Dropdown options for email status
 			?>
 			<select name="email_status" id="email_status">
@@ -160,13 +166,16 @@ class Logs_Output extends Base_Output {
 			</select>
 			<?php
 		}
+
 	}
 
 	/**
 	 * Modify the query based on the selected email status
 	 */
 	public function filter_email_logs_query_by_status( $query ) {
+
 		global $pagenow;
+
 		$post_type = isset( $_GET['post_type'] ) ? $_GET['post_type'] : '';
 
 		if ( $post_type == 'email_logs' && is_admin() && $pagenow == 'edit.php' && isset( $_GET['email_status'] ) && $_GET['email_status'] != '' ) {
@@ -174,53 +183,60 @@ class Logs_Output extends Base_Output {
 				array(
 					'key'   => 'status',
 					'value' => sanitize_text_field( $_GET['email_status'] ),
-				)
+				),
 			);
 			$query->set( 'meta_query', $meta_query );
 		}
+
 	}
 
 	/**
 	 * Restrict access to the email logs menu page
 	 */
 	public function restrict_access_to_email_logs() {
+
 		// Get the current screen object
-    	$screen = get_current_screen();
- 
+		$screen = get_current_screen();
+
 		// Check if the current screen is related to the 'email_logs' post type
 		if ( $screen && $screen->post_type === 'email_logs' ) {
 			// Check if the user has the 'manage_options' capability
-			if ( !current_user_can('manage_options') ) {
+			if ( ! current_user_can( 'manage_options' ) ) {
 				// If not, terminate the script with an error message
-				wp_die(__('You do not have sufficient permissions to access this page.', 'welcome-email-editor'));
-				
+				wp_die( __( 'You do not have sufficient permissions to access this page.', 'welcome-email-editor' ) );
+
 			}
 		}
+
 	}
 
 	/**
-	 * Hide the email logs menu page from the admin menu 
+	 * Hide the email logs menu page from the admin menu
 	 */
 	public function hide_email_logs_menu() {
+
 		// Check if the user does not have the 'manage_options' capability
-		if ( !current_user_can('manage_options') ) {
+		if ( ! current_user_can( 'manage_options' ) ) {
 			// Remove the 'email_logs' menu page
-			remove_menu_page('edit.php?post_type=email_logs');
+			remove_menu_page( 'edit.php?post_type=email_logs' );
 		}
+
 	}
 
 	/**
 	 * Add metabox to the email logs post type
 	 */
 	public function add_email_logs_metabox() {
+
 		add_meta_box(
-			'email_logs_metabox',      
-			__('Email Log Details'),   
-			array($this, 'email_logs_metabox_callback'),
-			'email_logs',              
-			'normal',                  
-			'high'                     
+			'email_logs_metabox',
+			__( 'Email Log Details' ),
+			array( $this, 'email_logs_metabox_callback' ),
+			'email_logs',
+			'normal',
+			'high'
 		);
+
 	}
 
 	/**
@@ -228,42 +244,44 @@ class Logs_Output extends Base_Output {
 	 *
 	 * @param [object] $post The post object.
 	 */
-	public function email_logs_metabox_callback($post) {
+	public function email_logs_metabox_callback( $post ) {
+
 		// Retrieve meta data
-		$email_type 			= get_post_meta($post->ID, 'email_type', true); 
-		$recipient 				= get_post_meta($post->ID, 'recipient', true);
-		$status 				= get_post_meta($post->ID, 'status', true);
-		$sender 				= get_post_meta($post->ID, 'sender', true);
-		$email_error_message 	= get_post_meta($post->ID, 'email_error_message', true);
+		$email_type          = get_post_meta( $post->ID, 'email_type', true );
+		$recipient           = get_post_meta( $post->ID, 'recipient', true );
+		$status              = get_post_meta( $post->ID, 'status', true );
+		$sender              = get_post_meta( $post->ID, 'sender', true );
+		$email_error_message = get_post_meta( $post->ID, 'email_error_message', true );
 
 		// Display the meta data in a table or any structured format
 		?>
 		<table class="form-table">
 			<tr>
-				<th><label for="email_type"><?php _e('Email Type'); ?></label></th>
-				<td><input type="text" id="email_type" value="<?php echo esc_attr($email_type); ?>" readonly /></td>
+				<th><label for="email_type"><?php _e( 'Email Type' ); ?></label></th>
+				<td><input type="text" id="email_type" value="<?php echo esc_attr( $email_type ); ?>" readonly /></td>
 			</tr>
 			<tr>
-				<th><label for="recipient"><?php _e('Recipient'); ?></label></th>
-				<td><input type="text" id="recipient" value="<?php echo esc_attr($recipient); ?>" readonly /></td>
+				<th><label for="recipient"><?php _e( 'Recipient' ); ?></label></th>
+				<td><input type="text" id="recipient" value="<?php echo esc_attr( $recipient ); ?>" readonly /></td>
 			</tr>
 		
 			<tr>
-				<th><label for="sender"><?php _e('Sender'); ?></label></th>
-				<td><input type="text" id="sender" value="<?php echo esc_attr($sender); ?>" readonly /></td>
+				<th><label for="sender"><?php _e( 'Sender' ); ?></label></th>
+				<td><input type="text" id="sender" value="<?php echo esc_attr( $sender ); ?>" readonly /></td>
 			</tr>
 			<tr>
-				<th><label for="status"><?php _e('Status'); ?></label></th>
-				<td><input type="text" id="status" value="<?php echo esc_attr($status); ?>" readonly /></td>
+				<th><label for="status"><?php _e( 'Status' ); ?></label></th>
+				<td><input type="text" id="status" value="<?php echo esc_attr( $status ); ?>" readonly /></td>
 			</tr>
-			<?php if ($email_error_message): // Only show this if there's an error ?>
+			<?php if ( $email_error_message ) : // Only show this if there's an error ?>
 			<tr>
-				<th><label for="email_error_message"><?php _e('Error Message'); ?></label></th>
-				<td><textarea id="email_error_message" rows="3" cols="40" readonly><?php echo esc_textarea($email_error_message); ?></textarea></td>
+				<th><label for="email_error_message"><?php _e( 'Error Message' ); ?></label></th>
+				<td><textarea id="email_error_message" rows="3" cols="40" readonly><?php echo esc_textarea( $email_error_message ); ?></textarea></td>
 			</tr>
 			<?php endif; ?>
 		</table>
 		<?php
+
 	}
 
 }
